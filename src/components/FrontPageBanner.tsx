858a459f4340f584cd8f26d44a08fa8085771c23
birkirfreyrg <birkirfrey@msn.com>
2023-05-01 02:22:41 +0000

    Banner and list created.

---
+++ src/components/FrontPageBanner.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/FrontPageBanner.css'
+
+function FrontPageBanner() {
+  return (
+    <div className="FrontPageBannerContainer">
+        <div className='FrontPageBanner'>
+            Banner
+        </div>
+   
+    </div>
+  );
+}
+
+export default FrontPageBanner;
---
+++ src/components/ListItemsPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/ListItemsPage.css'
+
+
+function ListItemsPage() {
+  return (
+    <div className="list-items-container">
+        <div className='list-items'>
+            items
+        </div>
+   
+    </div>
+  );
+}
+
+export default ListItemsPage;
